b882b57d35712aca83f8fe1b33bfaa0d3e0dcc8c
haazemsaid <haazemsaidd@gmail.com>
2024-09-08 22:52:00 +0300

    create the failures abstract class and  using either class to handling the errors

--- lib/features/home/data/reops/home_repo.dart
+++ lib/features/home/data/reops/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/util/errors/faliuers.dart';
+import 'package:bookly_app/features/home/data/model/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  FetchBestSellerbook();
-  Fetchfeaturebook();
+  Either<Failures, List<BookModel>> FetchBestSellerbook();
+  Either<Failures, List<BookModel>> Fetchfeaturebook();
 }
