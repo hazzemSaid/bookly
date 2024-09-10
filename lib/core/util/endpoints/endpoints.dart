da014610ec18cd3c16904da52ca3925e1f0e70ad
haazemsaid <haazemsaidd@gmail.com>
2024-09-11 00:14:27 +0300

    Refactor API service to use Endpoints for book requests

---
+++ lib/core/util/endpoints/endpoints.dart
@@ -0,0 +1,5 @@
+class Endpoints {
+  static const String featuredBooks = "volumes?q=search&filter=free-ebooks";
+  static const String newBooks =
+      "volumes?q=programming&filter=free-ebooks&sort=newest";
+}
--- lib/features/home/data/reops/home_repo_impl.dart
+++ lib/features/home/data/reops/home_repo_impl.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/util/api_servise.dart';
+import 'package:bookly_app/core/util/endpoints/endpoints.dart';
 import 'package:bookly_app/core/util/errors/faliuers.dart';
 import 'package:bookly_app/features/home/data/model/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/reops/home_repo.dart';
@@ -12,8 +13,7 @@ class HomeRepoImpl implements HomeRepo {
   @override
   Future<Either<Failures, List<BookModel>>> FetchNewestBooks() async {
     try {
-      var response = await apiServise
-          .get('volumes?q=subject:programming&filter=free-ebooks');
+      var response = await apiServise.get(Endpoints.newBooks);
       List<BookModel> books = [];
       for (var item in response['items']) {
         books.add(BookModel.fromJson(item));
@@ -28,8 +28,19 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> FetchFeaturedBooks() {
-    // TODO: implement Fetchfeaturebook
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> FetchFeaturedBooks() async {
+    try {
+      var response = await apiServise.get(Endpoints.featuredBooks);
+      List<BookModel> books = [];
+      for (var item in response['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailures.fromDioException(e));
+      }
+      return Left(ServerFailures(message: 'Something went wrong'));
+    }
   }
 }
