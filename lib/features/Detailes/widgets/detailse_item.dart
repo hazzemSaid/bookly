2469e60a1c105af6d97d171fdcb18d3198a29859
haazemsaid <haazemsaidd@gmail.com>
2024-09-20 08:59:21 +0300

    Refactor detailseItem to handle cases where the book's preview link is not available

--- lib/features/Detailes/widgets/detailse_item.dart
+++ lib/features/Detailes/widgets/detailse_item.dart
@@ -75,9 +75,31 @@ class detailseItem extends StatelessWidget {
         const SizedBox(
           height: 10,
         ),
-        customBottom(
-          book: book,
-        ),
+        book.volumeInfo?.previewLink != null
+            ? customBottom(book: book)
+            : Container(
+                margin: const EdgeInsets.only(top: 10),
+                height: 50,
+                width: 350,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 163, 34, 34),
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Not available',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+              )
       ],
     );
   }
